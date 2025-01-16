1f81f2f46f5ed1045dda440f81c726f89f34ee8a
Nicolas Cerecedo <nm.cerecedo@outlook.com>
2025-01-16 19:21:30 -0300

    confug

--- next.config.js
+++ next.config.js
@@ -6,4 +6,7 @@ module.exports = {
   env: {
     API_URL: process.env.API_URL || 'http://localhost:3000/api', // URL de la API
   },
+  eslint: {
+    ignoreDuringBuilds: true, // Desactiva ESLint durante el build
+  },
 };
