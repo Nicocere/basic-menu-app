0bfa9b1b3634cdc091d669951d97fb19317ff458
Nicolas Cerecedo <nm.cerecedo@outlook.com>
2025-01-16 19:50:12 -0300

    ssssssssssss

--- src/config/supabaseClient.js
+++ src/config/supabaseClient.js
@@ -4,11 +4,7 @@ const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 if (!supabaseUrl || !supabaseAnonKey) {
-    console.log(supabaseUrl, supabaseAnonKey);
   console.error('Las variables de entorno no están definidas correctamente.');
-} else {
-  console.log('Supabase URL:', supabaseUrl);
-  console.log('Supabase Anon Key:', supabaseAnonKey);
 }
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
