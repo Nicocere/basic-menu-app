4be2474a401d3b130f2acf2ff80cf0ab77c7f40f
Nicolas Cerecedo <nm.cerecedo@outlook.com>
2025-03-11 14:30:36 -0300

    Changes Front

--- src/components/Nav/NavBar.js
+++ src/components/Nav/NavBar.js
@@ -82,7 +82,6 @@ export default function NavBar() {
               {isDarkMode ? <FaSun /> : <FaMoon />}
             </button>
             
-        
           </div>
         </div>
       </header>
